69f6ba1bcc00b2068778d232c34736285516acaf
Godari <srinivas.godari@accenture.com>
2020-04-24 07:50:27 +0530

    my test code proct

---
+++ tests/ustc.js
@@ -0,0 +1,14 @@
+describe('demo calculator', function(){
+
+    it('addition testing', function(){
+        browser.get('https://juliemr.github.io/protractor-demo/');
+        element(by.model('first')).sendKeys('20');
+        element(by.model('second')).sendKeys('30');
+        element(by.css('[ng-click="doAddition()"]')).click();
+        let result = element(by.cssContainingText('.ng-binding','50')).click();
+        console.log("result displayed");
+        expect(result.getText()).toEqual('455');
+        browser.sleep(5000);
+    });
+    
+    });
